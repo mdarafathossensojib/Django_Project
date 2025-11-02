f98b594d0ded5842536c3374f43a8140deac4f39
ARAFAT <mdarafathossen.py@gmail.com>
2025-11-02 23:12:06 +0600

    Create Table task

--- tasks/models.py
+++ tasks/models.py
@@ -7,3 +7,4 @@ class Task(models.Model):
     is_complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
