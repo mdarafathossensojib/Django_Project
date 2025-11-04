cc713d4a5d46234a168a258886b2e7cb5acd40d9
ARAFAT <mdarafathossen.py@gmail.com>
2025-11-04 13:09:54 +0600

    debug toolbar added

--- task_managment/settings.py
+++ task_managment/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'users',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    # for hide debug toolbar when using docker
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'task_managment.urls'
