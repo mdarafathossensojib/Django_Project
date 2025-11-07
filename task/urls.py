e6fca6ddef7e63632a93cc4bf15ee91fb525ce98
ARAFAT <mdarafathossen.py@gmail.com>
2025-11-07 17:14:51 +0600

    new tailwind

---
+++ task/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('manager-dashboard/', manager_dashboard, name='manager-dashboard'),  
+    path('user-dashboard/', user_dashboard, name='user-dashboard'),  
+]
---
+++ task/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def manager_dashboard(request):
+    return render(request, 'dashboard/manager-dashboard.html')
+
+
+def user_dashboard(request):
+    return render(request, 'dashboard/user-dashboard.html')
