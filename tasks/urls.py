4d95655e14e2a60018ffd7503207220640740594
ARAFAT <mdarafathossen.py@gmail.com>
2025-11-07 15:59:15 +0600

    reset

--- tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import manager_dashboard, user_dashboard, test
-
-urlpatterns = [
-    path("manager-dashboard/", manager_dashboard),
-    path("user-dashboard/", user_dashboard),
-    path("test/", test),
-]
